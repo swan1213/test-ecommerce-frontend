97884d59c37f5de45730222cf96f444049519b4e
swan1213 <labonnomojibulislam@gmail.com>
2025-07-18 05:16:17 +0300

    fix: style issue

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,8 +5,6 @@ import { addCart } from "../redux/action";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
-import { Link } from "react-router-dom";
-import toast from "react-hot-toast";
 import ProductCard from "./ProductCard";
 import "./ProductCard.css";
 
